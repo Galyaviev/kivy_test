b653668fdef024ebd7ab01e3e68d0deebf5a3076
Ruslan <ruslan@ipcom.su>
2021-07-01 15:19:04 +0300

    wwww

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 
 
-class MainApp(MDApp):
+class MainApp1(MDApp):
     def build(self):
         return MDLabel(text="Hello, World", halign="center")
 
 
-MainApp().run()
+MainApp1().run()
